370ac4a93b98d8828367c6625807339b840f9f9c
jtl9479 <fbdrkd327@naver.com>
2023-08-11 18:08:33 +0900

    soild

--- java객체지향디자인패턴/chapter2객체지향원리/src/test.java
+++ java객체지향디자인패턴/chapter2객체지향원리/src/test.java
@@ -2,7 +2,9 @@ package java객체지향디자인패턴.chapter2객체지향원리.src;
 
 public class test {
     
-    public static void main(String[] args) {
-        System.out.println("sdadasdasd");
+    String ab = "1111";
+
+    public int aaa() {
+        return 1 + 2 ;
     }
 }
